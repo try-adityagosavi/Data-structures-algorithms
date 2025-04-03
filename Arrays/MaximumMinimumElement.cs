9da02e0bf885884032cb43d1222048bb661f1d0d
try-adityagosavi <try.adityagosavi@gmail.com>
2025-04-03 19:57:12 +0530

    pg 3 - processing element in pairs to reduce fewer comparison

--- Arrays/MaximumMinimumElement.cs
+++ Arrays/MaximumMinimumElement.cs
@@ -32,6 +32,57 @@ public class Program
 		return (max, min);
 	}
 	
+	// Efficient approach - Process elements in pairs - O(n) with fewer comparisons
+	public static (int, int) FindMinMaxPairwise(int[] arr)
+	{
+		int n = arr.Length;
+
+		if (n == 0)
+			return (Int32.MinValue, Int32.MaxValue);
+
+		int min, max;
+		int i = 0;
+
+		if (n % 2 == 0) // Even
+		{
+			if (arr[0] < arr[1])
+			{
+				min = arr[0];
+				max = arr[1];
+			}
+			else
+			{
+				min = arr[1];
+				max = arr[0];
+			}
+			i = 2;
+		}
+		else // Odd
+		{
+			min = max = arr[0];
+			i = 1;
+		}
+
+		while (i < n - 1)
+		{
+			if (arr[i] < arr[i + 1])
+			{
+				min = Math.Min(min, arr[i]);
+				max = Math.Max(max, arr[i + 1]);
+			}
+			else
+			{
+				min = Math.Min(min, arr[i + 1]);
+				max = Math.Max(max, arr[i]);
+			}
+
+			i += 2;
+		}
+
+		return (max, min);
+	}
+	
+	
 	public static void Main()
 	{
 		Console.WriteLine("Enter size of array : ");
@@ -67,7 +118,9 @@ public class Program
 		
 		Console.WriteLine();
 		
-		var (maxElement, minElement) = FindMaxLinearSearch(arr);
+		//var (maxElement, minElement) = FindMaxLinearSearch(arr);
+		
+		var (maxElement, minElement) = FindMinMaxPairwise(arr);
 		
 		Console.WriteLine($"Maximum element : {maxElement}");
 		Console.WriteLine($"Minimum element : {minElement}");
